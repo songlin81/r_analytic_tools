0ecd2f444b225bacf0942bebee491ebd46b29a39
Lin Song <song.lin@volvo.com>
2021-02-09 19:04:03 +0800

    clean up added Ridge and Lasso
    clean up added Ridge and Lasso

--- Regression/Regression.R
+++ Regression/Regression.R
@@ -151,6 +151,61 @@ voicelm <- glm(label~.,data = voicetrain,family = "binomial")
 summary(voicelm)
 
 
+voicelmstep <- step(voicelm,direction = "both")
+summary(voicelmstep)
+stepanova <- voicelmstep$anova
+stepanova$Step <- as.factor(stepanova$Step)
+ggplot(stepanova,aes(x = reorder(Step,-AIC),y = AIC))+
+  theme_bw(base_family = "STKaiti",base_size = 12)+
+  geom_point(colour = "red",size = 2)+
+  geom_text(aes(y = AIC-1,label = round(AIC,2)))+
+  theme(axis.text.x = element_text(angle = 30,size = 12))+
+  labs(x = "Deleted attributes")
+
+install.packages("forecast")
+library(forecast)
+voicelmpre <- predict(voicelm,voicetest,type = "response")
+voicelmpre2 <- as.factor(ifelse(voicelmpre > 0.5,1,0))
+voicesteppre <- predict(voicelmstep,voicetest,type = "response")
+voicesteppre2 <- as.factor(ifelse(voicesteppre > 0.5,1,0))
+
+table(voicetest$label,voicelmpre2)
+table(voicetest$label,voicesteppre2)
+
+pr <- prediction(voicelmpre, voicetest$label)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+prfdf <- data.frame(x = prf@x.values[[1]],logitic = prf@y.values[[1]])
+## ROC
+pr <- prediction(voicesteppre, voicetest$label)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+prfdf$logiticstep <- prf@y.values[[1]]
+prfdf2 <- gather(prfdf,key="model",value="y",2:3)
+ggplot(prfdf2,aes(x= x,y = y,colour = model,linetype = model))+
+  theme_bw(base_family = "STKaiti")+geom_line(size = 1)+
+  theme(aspect.ratio=1)+
+  labs(x = "False Positive Rate",y = "True rate")
+
+##################################################################
+# Poisson Regression
+install.packages("glmnet")
+library(glmnet)
+
+poi_sim <- read.csv("Regression/data/poisson_sim.csv")
+poi_sim <- poi_sim[,2:4]
+poi_sim$prog <- factor(poi_sim$prog,levels=1:3, 
+                       labels=c("Average", "Academic", "Professional"))
+hist(poi_sim$num_awards)
+
+model <- glm(num_awards~.-1,data = poi_sim,family = poisson(link = "log"))
+summary(model)
+exp(coef(model))
+
+##################################################################
+# Ridge & Lasso Regression
+
+
+
+
 
 
 
